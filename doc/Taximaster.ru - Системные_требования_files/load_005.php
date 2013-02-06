0593be8e07064068381dd509029b44f3584d4ee0
aas092 <alexander.shorikov@gmail.com>
2013-02-07 01:14:26 +0400

    First commit

---
+++ doc/Taximaster.ru - Системные_требования_files/load_005.php
@@ -0,0 +1,3 @@
+var gaJsHost=(("https:"==document.location.protocol)?"https://ssl.":"http://www.");document.write(unescape("%3Cscript src='"+gaJsHost+"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));try{var pageTracker=_gat._getTracker("UA-9888612-3");pageTracker._trackPageview();}catch(err){};mw.loader.state({"site":"ready"});
+
+/* cache key: taximaster_hl:resourceloader:filter:minify-js:7:0f8b328f4c3cc35f663db9f258283e90 */
